5fe066e0ebf8c8a02f33dbff5de48ff69abbdada
Jun Kang <jun@ip-172-31-1-179.ap-northeast-2.compute.internal>
2016-03-26 08:45:31 +0000

    mutation extended

--- radiomics_TCGA_luac.R
+++ radiomics_TCGA_luac.R
@@ -2,6 +2,7 @@ library(NMF)
 library(ConsensusClusterPlus)
 library(Cairo)
 library(Biocomb)
+library(pROC)
 
 ########### Assign Radiomics data ####################################################
 CT <- read.delim('TCGA_result_romove_second_mass.txt')
@@ -22,13 +23,30 @@ data_mut <- unstack(mutation, ID ~ Hugo_Symbol)
 
 summary(data_mut)
 
+write.table(data_rm_NA$ID, file = 'ID.txt', quote = FALSE, sep = ' ',
+            row.names = FALSE,
+            col.names = FALSE) 
+
+################ CNV ###########################
+cnv <- read.delim('./cnv/gdac.broadinstitute.org_LUAD-TP.CopyNumber_Gistic2.Level_4.2015082100.0.0/all_thresholded.by_genes.txt')
+ID_cnv <- gsub('\\....\\....\\.....\\...', '', colnames(cnv)[-1:-3])
+stk11_cnv <- data.frame(ID = gsub('\\.', '-', ID_cnv), cnv = as.numeric(cnv[cnv$Gene.Symbol == 'STK11',-1:-3]))
+sum(stk11_cnv$cnv < -1)
+
+dataCT$ID %in% mutation_ID
 ### subset having mutation data ########
 
 dataCT <- CT[CT$ID %in% unique(mutation_ID),]
 dataCT$EGFR <- dataCT$ID %in% data_mut$EGFR
 dataCT$TP53 <- dataCT$ID %in% data_mut$TP53
 dataCT$KRAS <- dataCT$ID %in% data_mut$KRAS
+dataCT$BRAF <- dataCT$ID %in% data_mut$BRAF
+dataCT$KEAP1 <- dataCT$ID %in% data_mut$KEAP1
+dataCT$NF1 <- dataCT$ID %in% data_mut$NF1
+dataCT$SETD2 <- dataCT$ID %in% data_mut$SETD2
 
+####### STK11  mutation only one case #####################
+############## CNV data not complete about half cases #####
 
 clinical <- read.delim('./nationwidechildrens.org_clinical_patient_luad.txt') 
 summary(clinical)
@@ -51,6 +69,11 @@ dataCT$fusion [dataCT$ID == 'TCGA-50-8460'] <- 'EML4_ALK'
 exp_subtype <- read.csv('tcga.luad.gene.expression.subtypes.20121025.csv')
 
 dataCT <- merge(dataCT, exp_subtype, by.x = 'ID', by.y = 'sampleId', all.x = TRUE)
+
+######################################################
+############### STK11 ################################
+##### From TCGA data portal ##########################
+
 
 
 
@@ -88,9 +111,13 @@ data_rm_NA <- dataCT[complete.cases(dataCT[,5:165]),]
 ann <- data.frame(EGFR = factor(dataCT$EGFR)[complete.cases(dataCT[,5:165])],
                   TP53 = factor(dataCT$TP53)[complete.cases(dataCT[,5:165])],
                   KRAS = factor(dataCT$KRAS)[complete.cases(dataCT[,5:165])],
+                  BRAF = factor(dataCT$BRAF)[complete.cases(dataCT[,5:165])],
+                  KEAP1 = factor(dataCT$KEAP1)[complete.cases(dataCT[,5:165])],
+                  #STK11 = factor(dataCT$STK11)[complete.cases(dataCT[,5:165])],
+                  NF1 = factor(dataCT$NF1)[complete.cases(dataCT[,5:165])],
+                  SETD2 = factor(dataCT$SETD2)[complete.cases(dataCT[,5:165])]
                   #Fusion = factor(dataCT$fusion)[complete.cases(dataCT[,5:165])],
-                  exp_subtype = factor(dataCT$expression_subtype)[complete.cases(dataCT[,5:165])])
-
+)
 
 #'ward', 'single', 'complete', 'average', 'mcquitty', 'median', 'centroid'
 
@@ -208,7 +235,31 @@ for (i in 5:165){
   auc_KRAS <- rbind(auc_KRAS, 0.5 + abs(a -0.5))
 }
 
-auc_data <- t(cbind(auc_EGFR, auc_TP53, auc_KRAS))
+auc_BRAF <- c()
+for (i in 5:165){
+  a <- auc(data_rm_NA$BRAF, data_rm_NA[,i])
+  auc_BRAF <- rbind(auc_BRAF, 0.5 + abs(a -0.5))
+}
+
+auc_KEAP1 <- c()
+for (i in 5:165){
+  a <- auc(data_rm_NA$KEAP1, data_rm_NA[,i])
+  auc_KEAP1 <- rbind(auc_KEAP1, 0.5 + abs(a -0.5))
+}
+
+auc_NF1 <- c()
+for (i in 5:165){
+  a <- auc(data_rm_NA$NF1, data_rm_NA[,i])
+  auc_NF1 <- rbind(auc_NF1, 0.5 + abs(a -0.5))
+}
+
+auc_SETD2 <- c()
+for (i in 5:165){
+  a <- auc(data_rm_NA$SETD2, data_rm_NA[,i])
+  auc_SETD2 <- rbind(auc_SETD2, 0.5 + abs(a -0.5))
+}
+
+auc_data <- t(cbind(auc_EGFR, auc_TP53, auc_KRAS, auc_BRAF, auc_KEAP1, auc_NF1, auc_SETD2))
 
 
 CairoPDF(file = 'auc.pdf',
@@ -247,10 +298,19 @@ dev.off()
 tapply(auc_data[1,], results[[5]]$consensusClass, mean)
 tapply(auc_data[2,], results[[5]]$consensusClass, mean)
 tapply(auc_data[3,], results[[5]]$consensusClass, mean)
+tapply(auc_data[4,], results[[5]]$consensusClass, mean)
+tapply(auc_data[5,], results[[5]]$consensusClass, mean)
+tapply(auc_data[6,], results[[5]]$consensusClass, mean)
+tapply(auc_data[7,], results[[5]]$consensusClass, mean)
 
 tapply(auc_data[1,], results[[5]]$consensusClass, sd)
 tapply(auc_data[2,], results[[5]]$consensusClass, sd)
 tapply(auc_data[3,], results[[5]]$consensusClass, sd)
+tapply(auc_data[4,], results[[5]]$consensusClass, sd)
+tapply(auc_data[5,], results[[5]]$consensusClass, sd)
+tapply(auc_data[6,], results[[5]]$consensusClass, sd)
+tapply(auc_data[7,], results[[5]]$consensusClass, sd)
+
 
 ################# AUC significance ####################
 ########### http://www.inside-r.org/node/322637 #########
@@ -275,63 +335,32 @@ p.values=compute.auc.permutation(aucs=vauc,dattable=data_test,rep.num)
 cor_table_cluster_1 <- cor(d[,results[[5]]$consensusClass == 1])
 mean_pairwise_cor_1 <- apply(cor_table_cluster_1, 1, mean)
 medoid_1 <- colnames(d[,results[[5]]$consensusClass == 1])[mean_pairwise_cor == max(mean_pairwise_cor)]
+mean_cluster_cor_1 <- mean(cor_table_cluster_1)
+sd_cluster_cor_1 <- sd(cor_table_cluster_1)
 
 cor_table_cluster_2 <- cor(d[,results[[5]]$consensusClass == 2])
 mean_pairwise_cor_2 <- apply(cor_table_cluster_2, 1, mean)
 medoid_2 <- colnames(d[,results[[5]]$consensusClass == 2])[mean_pairwise_cor == max(mean_pairwise_cor)]
+mean_cluster_cor_2 <- mean(cor_table_cluster_2)
+sd_cluster_cor_2 <- sd(cor_table_cluster_2)
 
 cor_table_cluster_3 <- cor(d[,results[[5]]$consensusClass == 3])
 mean_pairwise_cor_3 <- apply(cor_table_cluster_3, 1, mean)
 medoid_3 <- colnames(d[,results[[5]]$consensusClass == 3])[mean_pairwise_cor == max(mean_pairwise_cor)]
+mean_cluster_cor_3 <- mean(cor_table_cluster_3)
+sd_cluster_cor_3 <- sd(cor_table_cluster_3)
 
 cor_table_cluster_4 <- cor(d[,results[[5]]$consensusClass == 4])
 mean_pairwise_cor_4 <- apply(cor_table_cluster_4, 1, mean)
 medoid_4 <- colnames(d[,results[[5]]$consensusClass == 4])[mean_pairwise_cor == max(mean_pairwise_cor)]
+mean_cluster_cor_4 <- mean(cor_table_cluster_4)
+sd_cluster_cor_4 <- sd(cor_table_cluster_4)
 
 cor_table_cluster_5 <- cor(d[,results[[5]]$consensusClass == 5])
 mean_pairwise_cor_5 <- apply(cor_table_cluster_5, 1, mean)
 medoid_5 <- colnames(d[,results[[5]]$consensusClass == 5])[mean_pairwise_cor == max(mean_pairwise_cor)]
+mean_cluster_cor_5 <- mean(cor_table_cluster_5)
+sd_cluster_cor_5 <- sd(cor_table_cluster_5)
 
 
 
-
-
-###################################################
-########### Wilcox test ###########################
-
-p_value_KRAS <- c()
-
-for (i in 5:165) {
-  a <- wilcox.test(dataCT[,i] ~ dataCT$KRAS)
-  p_value_KRAS <- append(p_value_KRAS, a$p.value)
-}
-
-
-t.test()
-p_value_KRAS <0.01
-colnames(dataCT)[17]
-
-hist(log(dataCT$Energy_val))
-boxplot(Energy_val ~ KRAS, dataCT)
-
-p_value_EGFR <- c()
-
-for (i in 5:165) {
-  a <- wilcox.test(dataCT[,i] ~ dataCT$EGFR)
-  p_value_EGFR <- append(p_value_EGFR, a$p.value)
-}
-
-sum(p_value_EGFR <0.01)
-
-p_value_TP53 <- c()
-for (i in 5:165) {
-  a <- wilcox.test(dataCT[,i] ~ dataCT$TP53)
-  p_value_TP53 <- append(p_value_TP53, a$p.value)
-}
-
-sum(p_value_TP53 <0.01)
-
-par(mfrow = c(4,4))
-for (i in 4:19) {
-  hist(dataCT[,i])
-}
